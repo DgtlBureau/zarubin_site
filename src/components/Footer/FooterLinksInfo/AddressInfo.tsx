c7904cf6d59f4b3f8d8679defd27266d981ed0f4
Vitaliy <gronzzz@gmail.com>
2025-07-21 18:05:00 -0400

    Update AddressInfo.tsx

--- src/components/Footer/FooterLinksInfo/AddressInfo.tsx
+++ src/components/Footer/FooterLinksInfo/AddressInfo.tsx
@@ -5,7 +5,7 @@ export const AddressInfo = () => {
     <div className='mt-[32px] flex flex-col gap-[20px] tablet:mt-[60px] laptop:mt-0 laptop:flex-row laptop:gap-[32px]'>
       <div className='flex flex-col gap-[12px] font-proxima'>
         <h4 className='tablet:leading-1 text-[18px] font-bold leading-[1.3] tablet:text-[20px]'>
-          Cary, NC 27519
+          Cary, NC 27519, USA
         </h4>
         <Link
           href='tel:+13055282091'
@@ -16,7 +16,7 @@ export const AddressInfo = () => {
       </div>
       <div className='flex flex-col gap-[12px] font-proxima'>
         <h4 className='tablet:leading-1 text-[18px] font-bold leading-[1.3] tablet:text-[20px]'>
-          Jurija Gagarina, Belgrade
+          Jurija Gagarina, Belgrade, Serbia
         </h4>
         <Link
           href='tel:+381637742270'
