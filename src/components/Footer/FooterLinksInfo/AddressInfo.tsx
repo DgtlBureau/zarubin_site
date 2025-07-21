e261b87ad9354aa2ef3b1221e79419462c547a28
Vitaliy <gronzzz@gmail.com>
2025-07-21 18:00:18 -0400

    Update AddressInfo.tsx

--- src/components/Footer/FooterLinksInfo/AddressInfo.tsx
+++ src/components/Footer/FooterLinksInfo/AddressInfo.tsx
@@ -5,13 +5,24 @@ export const AddressInfo = () => {
     <div className='mt-[32px] flex flex-col gap-[20px] tablet:mt-[60px] laptop:mt-0 laptop:flex-row laptop:gap-[32px]'>
       <div className='flex flex-col gap-[12px] font-proxima'>
         <h4 className='tablet:leading-1 text-[18px] font-bold leading-[1.3] tablet:text-[20px]'>
-          121 union mills Way Cary, NC 27519
+          Cary, NC 27519
         </h4>
         <Link
           href='tel:+13055282091'
           className='text-[16px] leading-[1.25] desktop:text-[20px] desktop:leading-[1.2]'
         >
-          {'+1 (305) 528 2091'}
+          {''}
+        </Link>
+      </div>
+      <div className='flex flex-col gap-[12px] font-proxima'>
+        <h4 className='tablet:leading-1 text-[18px] font-bold leading-[1.3] tablet:text-[20px]'>
+          Jurija Gagarina, Belgrade
+        </h4>
+        <Link
+          href='tel:+381637742270'
+          className='text-[16px] leading-[1.25] desktop:text-[20px] desktop:leading-[1.2]'
+        >
+          {''}
         </Link>
       </div>
     </div>
